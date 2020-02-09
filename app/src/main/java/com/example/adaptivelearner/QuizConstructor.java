1c71bb09b8db046cab818ad83d05bd5897c48829
Ferdbugs <ferdbugs@gmail.com>
2020-02-09 19:33:09 +0600

    Updated Quiz with Medium and Hard for Comms

--- app/src/main/java/com/example/adaptivelearner/QuizConstructor.java
+++ app/src/main/java/com/example/adaptivelearner/QuizConstructor.java
@@ -24,4 +24,36 @@ public class QuizConstructor {
         return  quiz;
     }
 
+    public List<QuizQuestions> getCommunicationMediumQuiz(){
+
+        quiz.add(new QuizQuestions("Signals are electromagnetic representations of?","Data","Memory","Transmission","Wi-FI","Data"));
+        quiz.add(new QuizQuestions("Signals can be categorized as:","Direct/Analog","Alternate/Direct","Analog/Digital","Strong/Weak","Analog/Digital"));
+        quiz.add(new QuizQuestions("Analog signals are propagated over?","twisted pair","coaxial cable","Wi-Fi","All of the Above","All of the Above"));
+        quiz.add(new QuizQuestions("After a certain distance, analog signals will?","Attenuate  ","Strengthen","Widen","Accelerate","Attenuate"));
+        quiz.add(new QuizQuestions("Positive Voltage Pulses and Negative Voltage pulses in a digital signal are represented by:","Alpha/Beta","Sigma/K","0/1","AC/DC","0/1"));
+        quiz.add(new QuizQuestions("Analog data can converted to digital using a ___","Codec","Hub","Modem","Encryptor","Codec"));
+        quiz.add(new QuizQuestions("Advantage of Digital Signals over Analog ?","Less Noise","Faster","Less Attenuation","Wider","Less Noise"));
+        quiz.add(new QuizQuestions("Digital bandwidth is determined using ?","Bit Interval/Rate","Strength","Attenuation","Frequency","Bit Interval/Rate"));
+        quiz.add(new QuizQuestions("Digital signal with a bit interval of 90 microseconds, what is its bandwidth ?","1/(90*10^-6)","1/(60*10^-9)","1*(90*10^-9)","1*(90*10^-6)","1/(90*10^-6"));
+        quiz.add(new QuizQuestions("Digital bandwidth is typically expressed in?","Bits per Second","Lambda","Milliseconds","Bytes","Bits per Second"));
+
+        return  quiz;
+    }
+
+    public List<QuizQuestions> getCommunicationHardQuiz(){
+
+        quiz.add(new QuizQuestions("A twisted pair consists of two ___ copper wires.","insulated","conductive","thick","electrified","insulated"));
+        quiz.add(new QuizQuestions("The twisting in a twisted pair tends to decrease the ?","Interference","Insulation","Conductivity","Thickness","Interference"));
+        quiz.add(new QuizQuestions("Twisted Pair is susceptible to:","Interference","Noise","Both","None","Both"));
+        quiz.add(new QuizQuestions("Uses total internal reflection to transmit light?","Twisted Pair","Optical Fibre","Coaxial Cable","None","Optical Fibre"));
+        quiz.add(new QuizQuestions("Coupling of signal from one pair to another is known as:","Fading","Echo","Crosstalk","Feedback","Crosstalk"));
+        quiz.add(new QuizQuestions("Coaxial cable supports ___ bandwidths than twisted pair.","Higher","Lower","Equal","Separate","Higher"));
+        quiz.add(new QuizQuestions("Coaxial cable resists EMI better than bandwidths than?","Twisted Pair","Optical Fibre","Both","None","Twisted Pair"));
+        quiz.add(new QuizQuestions("Repeater spacing required for Fibre Optic cable?","1 KM","4 KM","7 KM",">10 KM",">10 KM"));
+        quiz.add(new QuizQuestions("Optical fibre is suitable for?","Long Distances","Short Distances","Both","Unlimited","Short Distances"));
+        quiz.add(new QuizQuestions("Optical fibre is immune to?","Cutting","Disconnecting","Interference","Delay","Interference"));
+
+        return  quiz;
+    }
+
 }
