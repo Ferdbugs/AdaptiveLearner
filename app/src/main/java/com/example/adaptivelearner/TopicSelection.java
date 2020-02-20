22304c256d1dff10784757f2f1190ae46fec53d5
Ferdbugs <ferdbugs@gmail.com>
2020-02-20 22:15:50 +0800

    UI Changes Applied

--- app/src/main/java/com/example/adaptivelearner/TopicSelection.java
+++ app/src/main/java/com/example/adaptivelearner/TopicSelection.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.Objects;
+
 public class TopicSelection extends AppCompatActivity {
 
     Button Communication;
